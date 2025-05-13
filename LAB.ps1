dbe7fa40f18cd6dd6ad7033480c9e14e183e6964
Tristan Faure <29113989+Issvn@users.noreply.github.com>
2025-05-13 11:52:09 +0000

    Added choice to run from local or from github

--- LAB.ps1
+++ LAB.ps1
@@ -5,13 +5,25 @@ $DOMAINDNS=NEVASEC.LOCAL
 # LDAP Root DN
 $LDAPROOT="DC=nevasec,DC=local"
 
-$c = @{ '1' = 'DC01'; '2' = 'SRV01'; '3' = 'PC01' }; 
-$s = Read-Host "Role to install:`n 1. Domain Controller (DC01)`n 2. Server (SRV01)`n 3. Client (PC01)`nEnter your choice"; 
+$a = Read-Host "Execution type: `n 1. Cloud (from github.com)`n 2. Local (need to download the repo)"
 
-if ($c.ContainsKey($s)) { 
-    (iex ("_scripts/" + $c[$s] + ".ps1") -PCNAME=$c[$s] -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT);
-#    (iwr -useb ("https://github.com/Issvn/LAB-AD/tree/main/_scripts/" + $c[$s] + ".ps1") -PCNAME=$c[$s] -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT); 
-    Invoke-LabSetup
+$c = @{ '1' = 'DC01'; '2' = 'SRV01'; '3' = 'PC01' }; 
+$s = Read-Host "`nRole to install:`n 1. Domain Controller (DC01)`n 2. Server (SRV01)`n 3. Client (PC01)`nEnter your choice"; 
+
+if ($a -eq '1' -or $a -eq 'Cloud') {
+    if ($c.ContainsKey($s)) { 
+        (iwr -useb ("https://github.com/Issvn/LAB-AD/tree/main/_scripts/" + $c[$s] + ".ps1") -PCNAME=$c[$s] -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT); 
+        Invoke-LabSetup
+    } else { 
+        Write-Host "Invalid Choice`n"
+    }
+} elseif ($a -eq '2' -or $a -eq 'Local') {
+    if ($c.ContainsKey($s)) { 
+        (iex ("_scripts/" + $c[$s] + ".ps1") -PCNAME=$($c[$s]) -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT);
+        Invoke-LabSetup
+    } else { 
+        Write-Host "Invalid Choice`n"
+    }
 } else { 
-    Write-Host "Invalid Choice"
+    Write-Host "Invalid Choice`n"
 }
