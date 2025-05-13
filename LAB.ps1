fa233150eb58320f45489349950f4f76850e0236
Tristan Faure <29113989+Issvn@users.noreply.github.com>
2025-05-13 11:38:03 +0000

    Small changes

--- LAB.ps1
+++ LAB.ps1
@@ -9,7 +9,8 @@ $c = @{ '1' = 'DC01'; '2' = 'SRV01'; '3' = 'PC01' };
 $s = Read-Host "Role to install:`n 1. Domain Controller (DC01)`n 2. Server (SRV01)`n 3. Client (PC01)`nEnter your choice"; 
 
 if ($c.ContainsKey($s)) { 
-    (iwr -useb ("https://github.com/Issvn/LAB-AD/tree/main/_scripts/" + $c[$s] + ".ps1") -PCNAME=$c[$s] -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT); 
+    (iex ("_scripts/" + $c[$s] + ".ps1") -PCNAME=$c[$s] -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT);
+#    (iwr -useb ("https://github.com/Issvn/LAB-AD/tree/main/_scripts/" + $c[$s] + ".ps1") -PCNAME=$c[$s] -DOMAIN=$DOMAIN -DOMAINDNS=$DOMAINDNS -LDAPROOT=$LDAPROOT); 
     Invoke-LabSetup
 } else { 
     Write-Host "Invalid Choice"
